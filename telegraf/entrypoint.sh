bf719c433aa63010cbaaedcff388ee8201a29c27
Tobias Schoch <Tobias.Schoch@helbling.ch>
2018-06-20 13:16:23 +0200

    added hostname forwarding

--- telegraf/entrypoint.sh
+++ telegraf/entrypoint.sh
@@ -2,8 +2,16 @@
 
 # replace env variables in configuration file
 echo "modify config file..."
-TELEGRAF_MQTT_PW=`cat run/secrets/telegraf_mqtt_pw`
-SER="s/<telegraf_mqtt_pw>/$(echo $TELEGRAF_MQTT_PW | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g"
+
+DOCKER_HOSTNAME=$(curl -XGET --unix-socket /var/run/docker.sock http://localhost/info | jq -r '.Name')
+SER="s/<hostname>/$(echo $DOCKER_HOSTNAME | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g"
 sed -i $SER /telegraf.conf
+
+if [ "$MQTT_CONSUMER" = "true" ]; then
+    cat mqtt_consumer.conf >> telegraf.conf
+    TELEGRAF_MQTT_PW=`cat run/secrets/telegraf_mqtt_pw`
+    SER="s/<telegraf_mqtt_pw>/$(echo $TELEGRAF_MQTT_PW | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g"
+    sed -i $SER /telegraf.conf
+fi
 
 exec "$@"
