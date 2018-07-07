d9131773fdd2f9ade54d060686fe4a0bdd5f2e99
Tobias <tobias.schoch@vtxmail.ch>
2018-07-08 01:57:13 +0200

    telegraf secrets management as for service

--- telegraf/entrypoint.sh
+++ telegraf/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # replace env variables in configuration file
-echo "use environment variables in config file telegraf_template.conf -> telegraf.conf"
-envsubst < telegraf_template.conf > telegraf.conf
+echo "modify config file..."
+TELEGRAF_MQTT_PW=`cat run/secrets/telegraf_mqtt_pw`
+SER="s/<telegraf_mqtt_pw>/$(echo $TELEGRAF_MQTT_PW | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g"
+sed -i $SER /telegraf.conf
 
 exec "$@"
