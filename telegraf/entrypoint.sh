743e4658a27d9e617ccb88a3e60d10dc8a7c8ed0
Tobias Schoch <tobias.schoch@geopraevent.ch>
2021-03-06 21:57:51 +0100

    telegraf config mounted, mqtt 2.0.8

--- telegraf/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# replace env variables in configuration file
-echo "modify config file..."
-TELEGRAF_MQTT_PW=`cat run/secrets/telegraf_mqtt_pw`
-SER="s/<telegraf_mqtt_pw>/$(echo $TELEGRAF_MQTT_PW | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g"
-sed -i $SER /telegraf.conf
-
-exec "$@"
